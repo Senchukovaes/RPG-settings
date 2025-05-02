e734a6b0ed3ffed125c213e4dd0444f2c132fdf2
senchukovaes <Senchha00@mail.ru>
2025-05-02 16:29:05 +0300

    Доделала всё

--- Vylonscript.js
+++
@@ -1,6 +0,0 @@
-function toggleCountry(card) {
-  const description = card.querySelector(".country-description");
-  const arrow = card.querySelector(".arrow");
-  description.classList.toggle("hidden");
-  arrow.classList.toggle("rotate");
-}
--- mainscript.js
+++ mainscript.js
@@ -17,3 +17,10 @@ function toggleSpells(id, element) {
   container.classList.toggle("hidden");
   arrow.classList.toggle("rotate");
 }
+
+function toggleCountry(card) {
+  const description = card.querySelector(".country-description");
+  const arrow = card.querySelector(".arrow");
+  description.classList.toggle("hidden");
+  arrow.classList.toggle("rotate");
+}
